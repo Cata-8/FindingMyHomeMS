05d42ee0c108e14091deb2c6039aba9bc721cc8d
Matias Pavez <mat.pavezb@duocuc.cl>
2026-05-03 20:29:33 -0400

    Repos

---
+++ UsuarioMS/src/main/java/cl/duoc/UsuarioMS/repository/RefugioRepository.java
@@ -0,0 +1,9 @@
+package cl.duoc.UsuarioMS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.duoc.UsuarioMS.model.Refugio;
+
+public interface RefugioRepository extends JpaRepository<Refugio, Integer>{
+
+}
