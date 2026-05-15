3c8a45c3488c0c83b14d17be64a2e8808abcde70
Cata-8 <catalinaalmuna01@gmail.com>
2026-05-15 19:53:56 -0400

    Add files via upload

---
+++ mascotaMS/src/main/java/cl/duoc/mascotaMS/MascotaMsApplication.java
@@ -0,0 +1,14 @@
+package cl.duoc.mascotaMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotaMsApplication.class, args);
+		System.out.println("hola");
+	}
+
+}
