c1416e185f91b37d053333ca3cacea1f9c575ed2
Matias Pavez <mat.pavezb@duocuc.cl>
2026-05-17 22:36:10 -0400

    .....

--- HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/dto/MascotaDTO.java
+++ HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/dto/MascotaDTO.java
@@ -1,8 +1,17 @@
 package cl.Duoc.HistorialAdopcionMS.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MascotaDTO {
+
+    private Integer id;
+    private String nombre;
+    private String tipo;
+    private String estado;
 
 }
--- HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/dto/UsuarioDTO.java
+++ HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/dto/UsuarioDTO.java
@@ -1,8 +1,18 @@
 package cl.Duoc.HistorialAdopcionMS.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioDTO {
+
+    private Integer idUsuario;
+    private String nombre;
+    private String apellido;
+    private String email;
+
 
 }
