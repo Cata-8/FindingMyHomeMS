c8325c18db1de837cab21ba413d384368876f3be
Matias Pavez <mat.pavezb@duocuc.cl>
2026-05-17 22:58:48 -0400

    .....

---
+++ HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/config/DataLoader.java
@@ -0,0 +1,30 @@
+package cl.Duoc.HistorialAdopcionMS.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cl.Duoc.HistorialAdopcionMS.model.HistorialAdopcion;
+import cl.Duoc.HistorialAdopcionMS.repository.HistorialRepository;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner initData(HistorialRepository repo){
+
+         return args -> {
+
+            HistorialAdopcion h1 = new HistorialAdopcion();
+            h1.setIdUsuario(1);
+            h1.setIdMascota(2);
+
+            HistorialAdopcion h2 = new HistorialAdopcion();
+            h2.setIdUsuario(2);
+            h2.setIdMascota(3);
+
+            repo.save(h1);
+            repo.save(h2);
+         };
+    }
+}
--- HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/controller/HistorialController.java
+++ HistorialAdopcionMS/src/test/java/cl/Duoc/HistorialAdopcionMS/controller/HistorialController.java
@@ -1,5 +1,73 @@
 package cl.Duoc.HistorialAdopcionMS.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.Duoc.HistorialAdopcionMS.dto.HistorialDTO;
+import cl.Duoc.HistorialAdopcionMS.model.HistorialAdopcion;
+import cl.Duoc.HistorialAdopcionMS.service.HistorialService;
+
+@RestController
+@RequestMapping("/api/v1/historial")
 public class HistorialController {
+
+    @Autowired
+    private HistorialService historialService;
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody HistorialDTO dto) {
+
+        try {
+
+            HistorialAdopcion nuevo = historialService.crearHistorial(dto);
+
+            return ResponseEntity.ok(nuevo);
+
+        } catch (Exception e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listar() {
+
+        return ResponseEntity.ok(historialService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+
+        try {
+
+            return ResponseEntity.ok(historialService.buscarPorId(id));
+
+        } catch (Exception e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+
+        try {
+
+            historialService.eliminar(id);
+
+            return ResponseEntity.ok("Historial eliminado");
+
+        } catch (Exception e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
