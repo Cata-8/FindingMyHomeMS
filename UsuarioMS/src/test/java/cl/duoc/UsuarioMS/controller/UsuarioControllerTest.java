65f8c28cc7b6629abdcb25176426758896aa4508
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-10 12:30:09 -0400

    agregando pruebas

--- UsuarioMS/src/test/java/cl/duoc/UsuarioMS/controller/UsuarioControllerTest.java
+++ UsuarioMS/src/test/java/cl/duoc/UsuarioMS/controller/UsuarioControllerTest.java
@@ -1,5 +1,61 @@
 package cl.duoc.UsuarioMS.controller;
 
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import ch.qos.logback.core.joran.action.Action;
+import cl.duoc.UsuarioMS.model.Usuario;
+import cl.duoc.UsuarioMS.service.UsuarioService;
+
+@WebMvcTest(UsuarioController.class) //levanta capa web no la base de datos
 public class UsuarioControllerTest {
 
+    @Autowired
+    private MockMvc mock; //simala las peticiones http
+
+    @Mock
+    private UsuarioService serviceU; //service falso 
+
+    private Usuario usuarioEjemplo;
+
+    @BeforeEach
+    void setUp(){
+
+        usuarioEjemplo = new Usuario(); 
+        usuarioEjemplo.setIdUsuario(1);
+        usuarioEjemplo.setNombre("Juan");
+        usuarioEjemplo.setApellido("Pérez");
+        usuarioEjemplo.setTelefono("2353636322");
+        usuarioEjemplo.setEmail("juanperez@gmail.com");
+    }
+
+
+
+    @Test
+    void buscarUsuario_retorna200(){
+        //ARRANGE = debe retornar usuario
+        when(serviceU.buscarUsuario(1)).thenReturn(usuarioEjemplo);
+
+        //ACT + ASSERT
+        mock.perform(get("/api/v1/usuarios/1")).andExpect(status().isOK());
+
+    }
+
+
+
+
+    @Test
+    void buscarUsuario_retorna404(){
+        //ARRANGE = no debe retornar usuario
+        when(serviceU.buscarUsuario(99)).thenReturn(usuarioEjemplo);
+
+        //ACT + ASSERT
+        mock.perform(get("/api/v1/usuarios/99")).andExpect(status().isOK());
+
+    }
 }
