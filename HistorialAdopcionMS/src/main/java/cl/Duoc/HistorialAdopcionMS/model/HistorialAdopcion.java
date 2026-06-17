74647fbc9508d7b614bc4279627ad3251c94f3eb
Cata-8 <catalinaalmuna01@gmail.com>
2026-06-16 20:49:21 -0400

    schema model

--- HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/model/HistorialAdopcion.java
+++ HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/model/HistorialAdopcion.java
@@ -2,6 +2,7 @@ package cl.Duoc.HistorialAdopcionMS.model;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,19 +19,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "historialAdopcion")
+@Schema(description = "Representa el historial de adopciones realizadas")
 public class HistorialAdopcion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID de historial de adopción", example = "1")
     private Integer idHistorial;
 
     @Column(nullable = false)
+    @Schema(description = "Fecha de cuando fue relizada la adopción")
     private LocalDate fechaAdopcion;
     
     @Column(nullable = false)
+    @Schema(description = "ID de usuario adoptante")
     private Integer idUsuario;
 
     @Column(nullable = false)
+    @Schema(description = "ID d mascota adoptada")
     private Integer idMascota;
 
     @PrePersist
--- notificacionMS/src/main/java/cl/duoc/notificacionMS/model/Notificacion.java
+++ notificacionMS/src/main/java/cl/duoc/notificacionMS/model/Notificacion.java
@@ -2,6 +2,7 @@ package cl.duoc.notificacionMS.model;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,22 +18,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "notificacion")
+@Schema(description = "Representa notificaciones enviadas a los usuarios")
 public class Notificacion {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID de la notificacion, solo permite datos numericos y se autoincrementa")
     private Integer id;
 
-
     @Column(nullable = false)
+    @Schema(description = "Mensaje que contiene la notificación")
     private String mensaje;
 
 
     @Column(nullable = false)
+    @Schema(description = "Fecha de emisión de la notificación")
     private Date fechaEmision;
 
 
     @Column(name = "usuario_id", nullable = false)
+    @Schema(description = "ID de usuario destinatario")
     private Integer usuarioId;
 
 }
