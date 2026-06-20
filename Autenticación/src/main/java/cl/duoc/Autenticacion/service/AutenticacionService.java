ad8e67db690d4756688a7f7335d0b95625620e68
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 16:41:06 -0400

    Cambios en Service

--- Autenticación/src/main/java/cl/duoc/Autenticacion/service/AutenticacionService.java
+++ Autenticación/src/main/java/cl/duoc/Autenticacion/service/AutenticacionService.java
@@ -5,7 +5,9 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cl.duoc.Autenticacion.Client.UsuarioClient;
 import cl.duoc.Autenticacion.dto.AutenticacionDTO;
+import cl.duoc.Autenticacion.dto.UsuarioDTO;
 import cl.duoc.Autenticacion.model.Autenticacion;
 import cl.duoc.Autenticacion.repository.AutenticacionRepository;
 
@@ -15,36 +17,41 @@ public class AutenticacionService {
     @Autowired
     private AutenticacionRepository authRepo;
 
+    @Autowired
+    private UsuarioClient usuarioClient;
 
-    public Autenticacion registrar(AutenticacionDTO dto){
+    public Autenticacion registrar(AutenticacionDTO dto) {
+        UsuarioDTO usuario = usuarioClient.buscarUsuario(dto.getIdUsuario());
+        if (usuario == null) {
+            throw new RuntimeException("El usuario no existe en UsuarioMS");
+        }
 
         Autenticacion existente = authRepo.findByIdUsuario(dto.getIdUsuario());
-
-        if(existente != null){
-        return existente; 
-        }   
+        if (existente != null) {
+            return existente;
+        }
 
         Autenticacion auth = new Autenticacion();
         auth.setIdUsuario(dto.getIdUsuario());
         auth.setEstado("activo");
-
         return authRepo.save(auth);
     }
 
-
     public String login(AutenticacionDTO dto) {
+        UsuarioDTO usuario = usuarioClient.buscarUsuario(dto.getIdUsuario());
+        if (usuario == null) {
+            return "Usuario no encontrado en UsuarioMS";
+        }
 
         Autenticacion auth = authRepo.findByIdUsuario(dto.getIdUsuario());
-
-        if(auth == null){
+        if (auth == null) {
             return "Usuario no encontrado";
         }
-        if(!auth.getEstado().equals("activo")){
+        if (!auth.getEstado().equals("activo")) {
             return "Usuario bloqueado";
         }
         auth.setUltimoLogin(LocalDateTime.now());
         authRepo.save(auth);
-
         return "Login exitoso";
     }
 
