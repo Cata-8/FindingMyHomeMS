e6ddd0309b712a0979e921476409516f45937435
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-19 23:20:06 -0400

    Creacion de Swagger y documentacion de DTO

--- publicacionMS/src/main/java/cl/duoc/publicacionMS/dto/MascotaDTO.java
+++ publicacionMS/src/main/java/cl/duoc/publicacionMS/dto/MascotaDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.publicacionMS.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Datos basicos de una mascota, obtenidos desde el microservicio de Mascota")
 public class MascotaDTO {
 
+    @Schema(description = "Identificador único de la mascota")
     private Integer id;
+
+    @Schema(description = "Nombre de la mascota")
     private String nombre;
+
+    @Schema(description = "Se clasifica de que especie es el animal, ya sea perro o gato")
     private String tipo;
 }
--- publicacionMS/src/main/java/cl/duoc/publicacionMS/dto/PublicacionDTO.java
+++ publicacionMS/src/main/java/cl/duoc/publicacionMS/dto/PublicacionDTO.java
@@ -2,6 +2,7 @@ package cl.duoc.publicacionMS.dto;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos basicos de una publicación de adopción")
 public class PublicacionDTO {
 
+    @Schema(description = "ID de publicación")
     private Integer id;
+
+    @Schema(description = "Título de la publicación")
     private String titulo;
+
+    @Schema(description = "Fecha en la que se realizó la publicación")
     private Date fechaPublicacion;
 
+    @Schema(description = "ID de mascota asociada a la publicación")
     private Integer idMascota;
 }
