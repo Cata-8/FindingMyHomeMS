a28d1876330546c1edad23bb64107e7e5bdf6022
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 15:43:06 -0400

    Documentacion de DTO Mensajeria

--- mensajeriaMS/src/main/java/cl/duoc/mensajeriaMS/dto/MensajeDTO.java
+++ mensajeriaMS/src/main/java/cl/duoc/mensajeriaMS/dto/MensajeDTO.java
@@ -2,7 +2,7 @@ package cl.duoc.mensajeriaMS.dto;
 
 import java.time.LocalDate;
 
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,17 +10,24 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos de un mensaje enviado entre usuarios")
 public class MensajeDTO {
 
+    @Schema(description = "ID del mensaje")
     private Integer id;
 
+    @Schema(description = "ID del remitente")
     private Integer idRemitente;
     
+    @Schema(description = "Contenido del mensaje")
     private String contenido;
 
+    @Schema(description = "ID del destinatario")
     private Integer idDestinatario;
 
+    @Schema(description = "Fecha de envío del mensaje")
     private LocalDate fechaEnvio;
     
+    @Schema(description = "Indica si el mensaje fue leído")
     private boolean leido;
 }
