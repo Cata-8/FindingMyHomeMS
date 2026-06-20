570923d9b2c59665cc9e5fcfcad9567b65a999f8
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 16:10:26 -0400

    Creacion de Swagger

---
+++ fichaSaludMS/src/main/java/cl/duoc/fichaSaludMS/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package cl.duoc.fichaSaludMS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+            .info(new Info()
+                .title("Microservicio gestión de ficha de salud")
+                .version("1.0")
+                .description("Documentación api que permite la gestión de fichas médicas de las mascotas")
+            );
+    }
+
+}
