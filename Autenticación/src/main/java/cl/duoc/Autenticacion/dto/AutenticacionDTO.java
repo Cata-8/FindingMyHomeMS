3a997854d9c106c386029b728e6ddb5d662a6bf1
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 16:44:06 -0400

    Documentacion de DTO

--- Autenticación/src/main/java/cl/duoc/Autenticacion/dto/AutenticacionDTO.java
+++ Autenticación/src/main/java/cl/duoc/Autenticacion/dto/AutenticacionDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.Autenticacion.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos necesarios para registrar o iniciar sesión de un usuario")
 public class AutenticacionDTO {
 
+    @Schema(description = "ID del usuario asociado")
     private Integer idUsuario;
 
 }
