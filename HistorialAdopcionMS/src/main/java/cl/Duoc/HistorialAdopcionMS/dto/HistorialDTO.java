7e3b53d7a27239d42cde0afc4f228c4d89a4f79f
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 15:55:13 -0400

    Documentacion de DTOs

--- HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/dto/HistorialDTO.java
+++ HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/dto/HistorialDTO.java
@@ -1,5 +1,6 @@
 package cl.Duoc.HistorialAdopcionMS.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos necesarios para registrar un historial de adopción")
 public class HistorialDTO {
 
+    @Schema(description = "ID de usuario adoptante")
     private Integer idUsuario;
+
+    @Schema(description = "ID de mascota")
     private Integer idMascota;
 }
--- HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/dto/MascotaDTO.java
+++ HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/dto/MascotaDTO.java
@@ -1,5 +1,6 @@
 package cl.Duoc.HistorialAdopcionMS.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos basicos de una mascota, obtenidos desde el microservicio de Mascota")
 public class MascotaDTO {
 
+    @Schema(description = "Identificador único de la mascota")
     private Integer id;
+
+    @Schema(description = "Nombre de la mascota")
     private String nombre;
+
+    @Schema(description = "Se clasifica de que especie es el animal, ya sea perro o gato")
     private String tipo;
+
+    @Schema(description = "Estado de la mascota, este puede ser 'Adoptado' o 'Disponible'")
     private String estado;
 
 }
