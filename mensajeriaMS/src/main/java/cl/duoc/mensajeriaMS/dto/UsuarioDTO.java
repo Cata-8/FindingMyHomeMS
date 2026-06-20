85bc3dc581f85bddd8e8481d24530b69d48ed17b
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 15:39:27 -0400

    Documentacion de model

--- mensajeriaMS/src/main/java/cl/duoc/mensajeriaMS/dto/UsuarioDTO.java
+++ mensajeriaMS/src/main/java/cl/duoc/mensajeriaMS/dto/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.mensajeriaMS.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Datos basicos de un usuario, obtenidos desde el microservicio de Usuario")
 public class UsuarioDTO {
 
+    @Schema(description = "Identificador único del usuario")
     private Integer idUsuario;
+
+    @Schema(description = "Nombre del usuario")
     private String nombre;
+
+    @Schema(description = "Apellido del usuario")
     private String apellido;
-    private String email;
 }
