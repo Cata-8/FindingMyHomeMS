578c232a0c4cd050797f6e07d787e288f5bad4b1
Matias Pavez <mat.pavezb@duocuc.cl>
2026-06-20 15:57:53 -0400

    Creacion de Swagger

---
+++ HistorialAdopcionMS/src/main/java/cl/Duoc/HistorialAdopcionMS/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package cl.Duoc.HistorialAdopcionMS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+            .info(new Info()
+                .title("Microservicio gestión de historial de adopción")
+                .version("1.0")
+                .description("Documentación api que permite la gestión del historial de adopciones")
+            );
+    }
+}
