757015dee938ca042c9c2dddf8e60db4c2e1300a
Cata-8 <catalinaalmuna01@gmail.com>
2026-06-21 21:33:37 -0400

    test

--- donacionMS/src/test/java/cl/duoc/donacionMS/controller/DonacionControllerTest.java
+++ donacionMS/src/test/java/cl/duoc/donacionMS/controller/DonacionControllerTest.java
@@ -6,19 +6,42 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import cl.duoc.donacionMS.model.Donacion;
+import cl.duoc.donacionMS.service.DonacionService;
 
 import org.springframework.http.MediaType;
 import static org.mockito.ArgumentMatchers.any;
 
 @WebMvcTest(DonacionController.class)
 public class DonacionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private DonacionService service;
+
+    private Donacion donacionEjemplo;
+    
+    @BeforeEach
+    void setUp(){
+        donacionEjemplo = new Donacion();
+        donacionEjemplo.setId(1);
+        donacionEjemplo.setMonto(20000);
+        donacionEjemplo.setFecha(new Date());
+        donacionEjemplo.setMetodoDePago("Tarjeta");
+        donacionEjemplo.setIdUsuario(2);
+    }
 
     
 }
